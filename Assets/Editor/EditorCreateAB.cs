90d8b1a194fd95d911cc8d4c86ce362cf97e7161
Joky8926 <262029232@qq.com>
2017-07-05 12:44:24 +0800

    AssetBundle

---
+++ Assets/Editor/EditorCreateAB.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class EditorCreateAB : EditorWindow {
+
+	[MenuItem("CreateAB/CreateMacAB")]
+	public static void CreateAssetBundleByMac() {
+		string sPath = "Assets/StreamingAssets";
+		BuildPipeline.BuildAssetBundles(sPath, BuildAssetBundleOptions.None, BuildTarget.StandaloneOSXUniversal);
+	}
+}
---
+++ Assets/Script/ShowCube.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowCube : MonoBehaviour {
+
+	void Start() {
+		CreateABPre();
+//		LoadABM();
+	}
+
+	private void CreateABPre() {
+		string sPath = "Assets/StreamingAssets/cube1";
+		AssetBundle ab = AssetBundle.LoadFromFile(sPath);
+		string sPath0 = "Assets/StreamingAssets/mt1";
+		AssetBundle.LoadFromFile(sPath0);
+		GameObject goPre = ab.LoadAsset<GameObject>("Cube1");
+		GameObject go = GameObject.Instantiate(goPre);
+		ab.Unload(true);
+//		Resources.UnloadAsset(goPre);
+//		GameObject go2 = GameObject.Instantiate(goPre);
+		go.transform.parent = this.transform;
+	}
+
+	private void LoadABM() {
+		string sPath = "Assets/StreamingAssets/StreamingAssets";
+		AssetBundle ab = AssetBundle.LoadFromFile(sPath);
+		AssetBundleManifest abm = ab.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+		string[] cubedepends = abm.GetAllDependencies("cube1");
+		Debug.Log(cubedepends[0]);
+	}
+
+	private void CreateLocalPre() {
+		GameObject goPre = Resources.Load<GameObject>("Cube2");
+		GameObject go = GameObject.Instantiate(goPre);
+		go.transform.parent = this.transform;
+	}
+}
