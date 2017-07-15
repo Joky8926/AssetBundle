ce8b4b99d757b9ed633fa67bd12183a047d474b9
Joky8926 <262029232@qq.com>
2017-07-15 18:24:34 +0800

    Test1

---
+++ Assets/Script/JList.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class JList : IEnumerable {
+	private List<string> lst = new List<string>();
+	private int uCurrIndex = -1;
+	string[] m_Days = { "Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat" };
+
+	public System.Collections.IEnumerator GetEnumerator()
+	{
+		for (uCurrIndex = 0; uCurrIndex < lst.Count; uCurrIndex++)
+		{
+			yield return lst[uCurrIndex];
+		}
+	}
+
+	public void Add(string value) {
+		lst.Add(value);
+	}
+
+	public void Remove(string value) {
+		lst.Remove(value);
+		uCurrIndex--;
+	}
+
+	public void RemoveAt(int uIndex) {
+		lst.RemoveAt(uIndex);
+	}
+
+	public void Dispose() {
+		
+	}
+}
---
+++ Assets/Script/TestForeach.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TestForeach : MonoBehaviour {
+	private JList lstStr;
+	private List<string> orcList;
+
+	void Start() {
+		InitList();
+		DoRemove();
+		OutputList();
+//		Other();
+	}
+
+	private void Other() {
+		orcList = new List<string>();
+		orcList.Add("aaa");
+		orcList.Add("bbb");
+		orcList.Add("bbb");
+		orcList.Add("ccc");
+		orcList.Add("ddd");
+		foreach (string str in orcList) {
+			if (str == "bbb") {
+				orcList.Add(str);
+			}
+		}
+		for (int i = 0; i < orcList.Count; i++) {
+			if (orcList[i] == "bbb") {
+				orcList.Remove(orcList[i]);
+				i--;
+			}
+		}
+		for (int i = 0; i < orcList.Count; i++) {
+			Debug.Log(orcList[i]);
+		}
+	}
+
+	private void InitList() {
+		lstStr = new JList();
+		lstStr.Add("aaa");
+		lstStr.Add("bbb");
+		lstStr.Add("bbb");
+		lstStr.Add("ccc");
+		lstStr.Add("ddd");
+	}
+
+	private void DoRemove() {
+		foreach (string str in lstStr) {
+			if (str == "bbb") {
+				lstStr.Remove(str);
+			}
+		}
+	}
+
+	private void OutputList() {
+		foreach (string str in lstStr) {
+			Debug.Log(str);
+		}
+	}
+}
